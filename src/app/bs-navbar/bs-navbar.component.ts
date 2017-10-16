059a0b5c66a3df4273ab02f10e4f096938d079f0
Yudong <gengyudonga@gmail.com>
2017-10-16 13:36:45 +0800

    Extracting a Service

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
-import { Observable } from 'rxjs/Rx';
+import { AuthService } from '../auth.service';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
@@ -9,17 +7,11 @@ import * as firebase from 'firebase';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
-    // afAuth.authState.subscribe(x => this.user = x);
+  constructor(public auth: AuthService) {
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
-
-
-
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
+import { AuthService } from '../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +10,10 @@ import * as firebase from 'firebase';
 })
 export class LoginComponent {
 
-  constructor(private afAuth: AngularFireAuth) { }
-
+  constructor(private authService: AuthService ) { }
 
   login() {
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-
+    this.authService.login();
   }
 
 }
