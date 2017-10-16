f053e10cb7b5e57a40642a064e311fc66890a00e
Yudong <gengyudonga@gmail.com>
2017-10-16 10:45:29 +0800

    Extracting NavBar Component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAORS1Og_KAcRVIEIdBFMJMZqG9jr58Of0',
+    authDomain: 'helloworldgengyudong.firebaseapp.com',
+    databaseURL: 'https://helloworldgengyudong.firebaseio.com',
+    projectId: 'helloworldgengyudong',
+    storageBucket: 'helloworldgengyudong.appspot.com',
+    messagingSenderId: '439092694146'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAORS1Og_KAcRVIEIdBFMJMZqG9jr58Of0',
+    authDomain: 'helloworldgengyudong.firebaseapp.com',
+    databaseURL: 'https://helloworldgengyudong.firebaseio.com',
+    projectId: 'helloworldgengyudong',
+    storageBucket: 'helloworldgengyudong.appspot.com',
+    messagingSenderId: '439092694146'
+  }
 };
