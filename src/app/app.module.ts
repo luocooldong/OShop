8612f3dbe6dac2d5600a89138beca8374852fc4c
Yudong <gengyudonga@gmail.com>
2017-10-16 15:58:27 +0800

    Redirecting after Logging In

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,9 +47,11 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component:  LoginComponent },
+
       { path: 'check-out', component: CheckOutComponent , canActivate: [AuthGuard] },
       { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       { path: 'my/orders', component:  MyOrdersComponent, canActivate: [AuthGuard] },
+
       { path: 'admin/products', component:  AdminProductsComponent, canActivate: [AuthGuard] },
       { path: 'admin/orders', component:  AdminOrdersComponent, canActivate: [AuthGuard] },
     ])
