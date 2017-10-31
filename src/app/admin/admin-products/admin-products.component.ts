e1f025e52c7ea596c7eae4b1e89cf80f42ded4b7
Yudong <gengyudonga@gmail.com>
2017-10-31 15:53:26 +0800

    Searching for Products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -7,30 +7,34 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  products: { title: string }[];
   filteredProducts: any[];
   subscription: Subscription;
 
   constructor(
     private productService: ProductService
   ) {
-    // console.log('fuck');
-    // this.subscription = this.productService.getAll().subscribe(
-    //   products =>  this.products$ = products
-    // );
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products
+    );
+    // this.products$ = this.productService.getAll();
   }
 
-  // fliter(query: string) {
-  //   console.log(query);
-  //   this.filteredProducts = (query) ?
-  //      this.products$.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
-  // }
+  filter(query: string) {
+    console.log(query);
+    this.filteredProducts = (query) ?
+       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+       this.products;
+  }
 
   ngOnInit() {
   }
 
-
+  ngOnDestroy() {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
+  }
 }
