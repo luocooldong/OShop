11b574852499e51cdbe5f0b3c827c1c647844226
Yudong <gengyudonga@gmail.com>
2017-10-31 15:00:00 +0800

    Displaying the List of Products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,21 +1,41 @@
+import { Subscription } from 'rxjs/Rx';
 import { ProductService } from '../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
+
   products$;
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(
     private productService: ProductService
   ) {
+    // console.log('fuck');
+    // this.subscription = this.productService.getAll().subscribe(
+    //   products =>  this.products$ = products
+    // );
     this.products$ = this.productService.getAll();
-   }
+  }
+
+  // fliter(query: string) {
+  //   console.log(query);
+  //   this.filteredProducts = (query) ?
+  //      this.products$.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  // }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
   }
 
 }
