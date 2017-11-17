6c8959d8faf71bc633c40026bb94bec1f08fdb2a
Yudong <gengyudonga@gmail.com>
2017-11-17 09:33:52 +0800

    displaying the totalPrice

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,17 +1,30 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
-    constructor (public items: ShoppingCartItem[]) {
+    items: ShoppingCartItem[] = [];
+    constructor (public itemsMap: { [productId: string] : ShoppingCartItem }) {
+      for(let productId in itemsMap) {
+        let item = itemsMap[productId];
+        // 遍历循环数组中的数据
+        this.items.push(new ShoppingCartItem(item.product, item.quantity));
+      }
     }
 
     get productIds() {
-      return Object.keys(this.items);
+      return Object.keys(this.itemsMap);
+    }
+
+    get totalPrice () {
+      let sum = 0;
+      for(let productId in this.items) 
+        sum += this.items[productId].totalPrice;
+      return sum;
     }
 
     get totalItemsCount () {
       let count = 0;
-      for (let productId in this.items) 
-        count += this.items[productId].quantity;
+      for (let productId in this.itemsMap) 
+        count += this.itemsMap[productId].quantity;
       return count;
     }
 }
